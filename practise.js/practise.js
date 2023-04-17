dc47ea113818fc10d2a83ee9c664c63c69adfe41
deepak7691 <kawliya11@gmail.com>
2023-04-17 13:39:25 +0530

    deepak

---
+++ practise.js/practise.js
@@ -0,0 +1,54 @@
+// console.log("line one");
+
+// setTimeout(() => {
+//     console.log("Hello Last me ")
+// }, 0);
+
+// for (let i = 1; i < 100; i++) {             // output >  line one   Hii 99 br   line last   Hello Last me
+
+//     console.log("Hii");
+
+// }
+
+// console.log("line last");
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log("start");
+
+// let id =setTimeout(() =>{
+//     console.log("I am going to run after 2 secs");
+// },2000)                                                     //Output > start  setTimeout Cleared  end
+//                                                             // 0 m sec hoga to bhi  yahi output aayega
+// clearTimeout(id);
+// console.log("setTimeout Cleared");
+// console.log("end")
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log("start");
+
+// let id =setInterval(() =>{
+//     console.log("I am going to run after 2 secs");
+// },2000)                                                     //Output > start  end  i am...  i am... i am... upto infinite
+//                                                             // 
+// console.log("end")
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log("start");
+
+// let id = setInterval(() => {
+//     console.log("I am going to run after 2 secs");
+// }, 2000)                                                     //Output > start  end  i am...  i am... i am... iam...
+
+
+// setTimeout(() => {
+//     clearInterval(id)
+// }, 10000)
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
